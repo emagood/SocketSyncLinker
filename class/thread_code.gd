54e0511e3d8581c3599f6c79490166db599300e6
emagood <emabertey@gmail.com>
2024-11-13 14:34:58 -0300

    add clase player , room , y rcp

--- class/thread_code.gd
+++ class/thread_code.gd
@@ -1,7 +1,8 @@
-extends class_code
+extends Node
 class_name class_thread
+
 var data_thread = {
-	"hola" : "emanuel",
+	"hola": "emanuel",
 	"genio": "servidor"
 }
 
@@ -10,56 +11,46 @@ var thread2: Thread
 var timer_local = 0
 var timer_local2 = 0
 
-
-
 func _ready():
 	thread = Thread.new()
 	thread2 = Thread.new()
 	timer_local = Time.get_ticks_msec()
 	timer_local2 = timer_local
-	prints(Time.get_ticks_msec())
-	# You can bind multiple arguments to a function Callable.
-	thread.start(_thread_function.bind(data_thread))
-	thread2.start(_thread_function.bind(data_thread))
-	
+	prints("Tiempo inicial: " + str(Time.get_ticks_msec()))
 
+	# Iniciar threads con datos vinculados
+	if not thread.is_alive():
+		thread.start(_thread_function.bind(data_thread))
+	if not thread2.is_alive():
+		thread2.start(_thread_function.bind(data_thread))
 
-# Run here and exit.
-# The argument is the bound data passed from start().
+# Función que ejecutará cada thread
 func _thread_function(userdata):
 	var a = 1000
-	while a :
+	while a:
 		prints("abuelo")
 		if userdata.has("hola"):
 			prints("ema")
-		#if userdata.has("genio"):
-			#prints("genio")
 		a -= 1
 
-
-
-	print("I'm a thread! Userdata is: ", userdata)
-	prints(Time.get_ticks_msec())
-	timer_local = Time.get_ticks_msec() 
-	prints("el tiempo paso " + str(timer_local - timer_local2))
-
-
+	prints("Soy un thread! Los datos de usuario son: ", userdata)
+	timer_local = Time.get_ticks_msec()
+	prints("El tiempo pasó: " + str(timer_local - timer_local2) + " ms")
 
 func _exit_tree():
-	prints("tread")
+	prints("Esperando a que los threads terminen")
 	thread.wait_to_finish()
 	thread2.wait_to_finish()
-
-
-
-
-
+	prints("Threads terminados")
 
 func _input(event: InputEvent) -> void:
-	if Input.is_key_pressed(KEY_W): 
+	if Input.is_key_pressed(KEY_W):
+		if not thread.is_alive():
 			thread.wait_to_finish()
 			thread = Thread.new()
-			thread.start(_thread_function.bind("soy-yo"))
+			thread.start(_thread_function.bind({"mensaje": "soy-yo"}))
+
+		if not thread2.is_alive():
 			thread2.wait_to_finish()
 			thread2 = Thread.new()
-			thread2.start(_thread_function.bind("momo"))
+			thread2.start(_thread_function.bind({"mensaje": "momo"}))
--- escena/thread/thread.gd
+++ escena/thread/thread.gd
@@ -1,6 +1,8 @@
-extends Control
+extends Node
+
+
 var data_thread = {
-	"hola" : "emanuel",
+	"hola": "emanuel",
 	"genio": "servidor"
 }
 
@@ -9,56 +11,56 @@ var thread2: Thread
 var timer_local = 0
 var timer_local2 = 0
 
-
-
-func _ready():
+func _init():
 	thread = Thread.new()
 	thread2 = Thread.new()
 	timer_local = Time.get_ticks_msec()
 	timer_local2 = timer_local
-	prints(Time.get_ticks_msec())
-	# You can bind multiple arguments to a function Callable.
-	thread.start(_thread_function.bind(data_thread))
-	thread2.start(_thread_function.bind(data_thread))
-	
 
+func _ready():
+	# Iniciar threads con datos vinculados
+	start_threads()
+	prints("Threads iniciados.")
 
-# Run here and exit.
-# The argument is the bound data passed from start().
+func start_threads():
+	if thread == null or not thread.is_alive():
+		thread = Thread.new()
+		thread.start(_thread_function.bind(data_thread))
+	if thread2 == null or not thread2.is_alive():
+		thread2 = Thread.new()
+		thread2.start(_thread_function.bind(data_thread))
+
+func stop_threads():
+	if thread != null and thread.is_alive():
+		thread.call_deferred("wait_to_finish")
+		thread = null
+	if thread2 != null and thread2.is_alive():
+		thread2.call_deferred("wait_to_finish")
+		thread2 = null
+	prints("Threads detenidos.")
+
+func restart_threads():
+	stop_threads()
+	start_threads()
+	prints("Threads reiniciados.")
+
+# Función que ejecutará cada thread
 func _thread_function(userdata):
 	var a = 1000
-	while a :
-		#prints("abuelo")
+	while a:
+		prints("abuelo")
 		if userdata.has("hola"):
-			pass
-		#if userdata.has("genio"):
-			#prints("genio")
+			prints("ema")
 		a -= 1
 
-
-
-	print("I'm a thread! Userdata is: ", userdata)
-	prints(Time.get_ticks_msec())
-	timer_local = Time.get_ticks_msec() 
-	prints("el tiempo paso " + str(timer_local - timer_local2))
-
-
+	prints("Soy un thread! Los datos de usuario son: ", userdata)
+	timer_local = Time.get_ticks_msec()
+	prints("El tiempo pasó: " + str(timer_local - timer_local2) + " ms")
 
 func _exit_tree():
-	prints("tread")
-	thread.wait_to_finish()
-	thread2.wait_to_finish()
-
-
-
-
-
+	stop_threads()
+	prints("Esperando a que los threads terminen.")
 
 func _input(event: InputEvent) -> void:
-	if Input.is_key_pressed(KEY_W): 
-			thread.wait_to_finish()
-			thread = Thread.new()
-			thread.start(_thread_function.bind(data_thread))
-			thread2.wait_to_finish()
-			thread2 = Thread.new()
-			thread2.start(_thread_function.bind(data_thread))
+	if Input.is_key_pressed(KEY_W):
+		restart_threads()
